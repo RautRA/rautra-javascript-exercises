53ac9246d23de744a9da5358a6161fa316002438
RautRA <168980384+RautRA@users.noreply.github.com>
2024-06-04 20:13:51 +0530

    Add solution for "02_repeatString" exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    const originalString = string;
 
+    if (num === 0) {
+        return '';
+    } else if (num < 0) {
+        return 'ERROR';
+    }
+
+    for(let i = 1; i < num; i++) {
+        string += originalString;
+    }
+
+    return string;
 };
 
 // Do not edit below this line
