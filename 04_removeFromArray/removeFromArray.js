d78d983824cbce9ad48fe3917ca56a1236ad1243
RautRA <168980384+RautRA@users.noreply.github.com>
2024-06-04 22:12:59 +0530

    Add solution for "04_removeFromArray" exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    args.forEach(arg => {
+        // keep looping to remove multiple elements with same value
+        while(array.indexOf(arg) !== -1) {
+            array.splice(array.indexOf(arg), 1);
+        }
+    });
+
+    return array;
 };
 
 // Do not edit below this line
