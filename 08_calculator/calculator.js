249c99bc41423741f9d00d7b628594f01f4dc4e8
RautRA <168980384+RautRA@users.noreply.github.com>
2024-06-12 22:40:58 +0530

    Add solution for "08_calculator" Exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((acc, cur) => acc + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((acc, cur) => acc * cur, 1);
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+
+  while(num >= 1) {
+    fact *= num;
+    num--;
+  }
+
+  return fact;
 };
 
 // Do not edit below this line
