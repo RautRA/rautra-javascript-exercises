e9da35ebd32337c364abc00615e3f2f640d6376e
RautRA <168980384+RautRA@users.noreply.github.com>
2024-07-03 22:23:16 +0530

    Add solution for "10_fibonacci" exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
 
+    if (num === 0) {
+        return 0;
+    } else if (num < 0) {
+        return "OOPS";
+    }
+
+    const fibonacciSeq = [];
+    for(let i = 0; i < num; i++) {
+        if (i <= 1) {
+            fibonacciSeq.push(1);
+        } else {
+            fibonacciSeq.push(fibonacciSeq[i - 1] + fibonacciSeq[i - 2]);
+        }
+    }
+
+    return fibonacciSeq[fibonacciSeq.length - 1];
 };
 
 // Do not edit below this line
