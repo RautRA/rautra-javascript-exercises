85f5528ac01f022755e174d18c21e511da345896
RautRA <168980384+RautRA@users.noreply.github.com>
2024-07-03 21:54:15 +0530

    Add solution for "09_palindromes" exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const stringArray = string.split("");
+    const reversedStringArray = [];
+    const punctuations = [];
+    const regex = /[^a-z0-9]/ig;
 
-};
+    for (let i = 0; i < stringArray.length; i++) {
+        if (regex.test(stringArray[i])) {
+            punctuations.push({character: stringArray[i], index: i});
+            continue;
+        }
+        reversedStringArray.unshift(stringArray[i]);
+    }
+    
+    punctuations.forEach(punctuation => {
+        reversedStringArray.splice(punctuation.index, 0, punctuation.character);
+    });
+
+    if (reversedStringArray.join("").toLowerCase().replace(/\s/g, "") === 
+        string.toLowerCase().replace(/\s/g, "")) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = palindromes;
